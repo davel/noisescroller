2176eb9def847014a6e383e6bd2bcb922aaa3937
Dave Lambley <github@davel.me.uk>
2019-06-22 19:25:40 +0100

    Try a LFSR.

--- rtl/videogen.v
+++ rtl/videogen.v
@@ -76,9 +76,9 @@ assign B_out = ENABLE_out ? V_gen : 8'h00;
 reg [7:0] V_gen;
 
 //reg [7:0] lfsr_frame;
-//reg [7:0] lfsr_pixel;
+reg [7:0] lfsr_pixel;
 //assign frame_feedback =  ! (lfsr_frame[7] ^ lfsr_frame[3]);
-//assign pixel_feedback =  ! (lfsr_pixel[7] ^ lfsr_pixel[3]);
+assign pixel_feedback =  ! (lfsr_pixel[7] ^ lfsr_pixel[3]);
 
 
 //HSYNC gen (negative polarity)
@@ -155,13 +155,13 @@ begin
             else if ((h_cnt < X_START+H_OVERSCAN+H_BORDER) || (h_cnt >= X_START+H_OVERSCAN+H_AREA-H_BORDER) || (v_cnt < Y_START+V_OVERSCAN+V_BORDER) || (v_cnt >= Y_START+V_OVERSCAN+V_AREA-V_BORDER))
                 V_gen <= 8'h50;
             else
-                V_gen <= h_cnt[4] ? 8'hff:8'h00;
+                V_gen <= lfsr_pixel[7] ? 8'h20:8'hdf;
 
-//                lfsr_pixel <= {
-//                    lfsr_pixel[6],lfsr_pixel[5],
-//                    lfsr_pixel[4],lfsr_pixel[3],
-//                    lfsr_pixel[2],lfsr_pixel[1],
-//                    lfsr_pixel[0], pixel_feedback};
+                lfsr_pixel <= {
+                    lfsr_pixel[6],lfsr_pixel[5],
+                    lfsr_pixel[4],lfsr_pixel[3],
+                    lfsr_pixel[2],lfsr_pixel[1],
+                    lfsr_pixel[0], pixel_feedback};
         end
 
         ENABLE_out <= (h_cnt >= X_START && h_cnt < X_START + H_ACTIVE && v_cnt >= Y_START && v_cnt < Y_START + V_ACTIVE);
